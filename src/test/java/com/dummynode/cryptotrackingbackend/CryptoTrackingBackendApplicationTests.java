ca0bb4ac1b0d6fb7326443832f5fe3a93b7f37be
MO HUAIHAO <mohuaihaoxm@gmail.com>
2024-10-30 10:12:44 -0400

    init
    Spring Boot Init
    JDK 17

---
+++ src/test/java/com/dummynode/cryptotrackingbackend/CryptoTrackingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dummynode.cryptotrackingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTrackingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
